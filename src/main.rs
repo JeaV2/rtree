5d902420be1e4255e1827d79f6fd9bced505077d
JeaV2 <102710@glr.nl>
2026-03-12 14:54:44 +0100

    feat: scan root or specified folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+mod config;
+
+fn main() -> io::Result<()> {
+    let args = config::get_args();
+    let path = if args.len() > 1 { &args[1] } else { "/" };
+    let files = fs::read_dir(path)?;
+    for file in files {
+        let file = file?;
+        let filename = file.file_name();
+        println!("{} D:{}", filename.to_string_lossy(), file.file_type()?.is_dir());
+    }
+    Ok(())
 }
