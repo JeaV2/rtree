02a5e3d55bb7c1f6e8e72c68326ebd090cf95532
JeaV2 <102710@glr.nl>
2026-03-24 14:09:49 +0100

    feat: code now scans through directories recursively

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use std::fs;
 use std::io;
+use std::path::Path;
+
 mod config;
+
+fn visit_dir(path: &Path, depth: usize) -> io::Result<()> {
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let entry_path = entry.path();
+        let name = entry.file_name();
+
+        println!("├{}{}", "─".repeat(depth), name.to_string_lossy());
+
+        if entry_path.is_dir() {
+            visit_dir(&entry_path, depth + 1)?;
+        }
+    }
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
     let args = config::get_args();
     let path = if args.len() > 1 { &args[1] } else { "/" };
-    let files = fs::read_dir(path)?;
-    for file in files {
-        let file = file?;
-        let filename = file.file_name();
-        println!("{} D:{}", filename.to_string_lossy(), file.file_type()?.is_dir());
-    }
-    Ok(())
+
+    println!(".");
+    visit_dir(Path::new(path), 0)
 }
