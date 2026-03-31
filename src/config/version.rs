7b30240f90b2f46c0c463191aecee960489d4d6b
JeaV2 <102710@glr.nl>
2026-03-31 11:44:22 +0200

    chore: format file

--- src/config/version.rs
+++ src/config/version.rs
@@ -1,6 +1,11 @@
 pub const LONG_VERSION: &str = concat!(
-    "Name: ", env!("CARGO_PKG_NAME"), "\n",
-    "Version: ", env!("CARGO_PKG_VERSION"), "\n",
-    "Description: ", env!("CARGO_PKG_DESCRIPTION"), "\n",
-    "License: ", env!("CARGO_PKG_LICENSE")
+    "\n",
+    "Version: ",
+    env!("CARGO_PKG_VERSION"),
+    "\n",
+    "Description: ",
+    env!("CARGO_PKG_DESCRIPTION"),
+    "\n",
+    "License: ",
+    env!("CARGO_PKG_LICENSE")
 );
